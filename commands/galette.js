ff5adef689bf5e10edc9b6111cd5e10ff562dac6
Alexis Launay <yorunohikage@gmail.com>
2021-11-28 01:01:25 +0100

    Add galette command

---
+++ commands/galette.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('galette')
+    .setDescription("Gloire à la Sainte Galette-Saucisse !"),
+  async execute(interaction) {
+    return interaction.reply(':galette:'.repeat(27));
+  },
+};

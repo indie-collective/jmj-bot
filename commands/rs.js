a3c1e2f8b2274a1a206ecee162067f2afeb4c11a
Alexis Launay <yorunohikage@gmail.com>
2021-11-28 01:01:34 +0100

    Add rs command

---
+++ commands/rs.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('rs')
+    .setDescription("Les réseaux sociaux d'Indie Collective !"),
+  async execute(interaction) {
+    return interaction.reply(`Hésitez pas à suivre notre merveilleuse association sur tous les réseaux sociaux :
+➡️ https://linktr.ee/indiecollective
+
+Mais surtout, suivez-moi sur Twitter <https://twitter.com/jeanmichel_jam> !`);
+  },
+};

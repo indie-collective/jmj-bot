293a6572feb434977e5f3ed7cf54d6f24a39783d
Alexis Launay <yorunohikage@gmail.com>
2022-01-30 15:39:57 +0100

    Queue events coming from the server

--- website/alerts/index.js
+++ website/alerts/index.js
@@ -9,12 +9,12 @@ function connect() {
     const { type, data } = JSON.parse(event.data);
 
     if (type === 'helloasso') {
-      playHelloAsso(data.name, data.donation, data.type);
+      Queue.enqueue(() => playHelloAsso(data.name, data.donation, data.type));
     } else if (type === 'twitch') {
       if (data.reward.title === 'JMJ') {
-        playJMJ();
+        Queue.enqueue(() => playJMJ());
       } else if (data.reward.title === 'Apprends-moi Jamy !') {
-        playHelpMeJamy(data.user_name);
+        Queue.enqueue(() => playHelpMeJamy(data.user_name));
       }
     }
   };
@@ -148,4 +148,49 @@ async function playHelpMeJamy(name) {
   // hide
   container.classList.remove('visible');
   tts.remove();
+}
+
+class Queue {
+  static queue = [];
+  static pendingPromise = false;
+
+  static enqueue(promise) {
+    return new Promise((resolve, reject) => {
+        this.queue.push({
+            promise,
+            resolve,
+            reject,
+        });
+        this.dequeue();
+    });
+  }
+
+static dequeue() {
+    if (this.workingOnPromise) {
+      return false;
+    }
+    const item = this.queue.shift();
+    if (!item) {
+      return false;
+    }
+    try {
+      this.workingOnPromise = true;
+      item.promise()
+        .then((value) => {
+          this.workingOnPromise = false;
+          item.resolve(value);
+          this.dequeue();
+        })
+        .catch(err => {
+          this.workingOnPromise = false;
+          item.reject(err);
+          this.dequeue();
+        })
+    } catch (err) {
+      this.workingOnPromise = false;
+      item.reject(err);
+      this.dequeue();
+    }
+    return true;
+  }
 }

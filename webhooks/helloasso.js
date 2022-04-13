172d023a5e21f3907871d0f02d594a20518b317d
Alexis Launay <yorunohikage@gmail.com>
2022-04-13 23:15:22 +0200

    Fix import order in helloasso webhook

--- webhooks/helloasso.js
+++ webhooks/helloasso.js
@@ -1,8 +1,9 @@
-const wss = require('../ws');
 const express = require('express');
-const router = express.Router();
 
+const wss = require('../ws');
 const helloasso = require('../helloasso');
+
+const router = express.Router();
 
 router.post('/helloasso', (req, res) => {
   const { data, eventType } = req.body;

137c0866b1a979b4eaa86e2b7479cc8a56f532d4
Alexis Launay <yorunohikage@gmail.com>
2024-08-03 17:36:40 +0200

    Fix helloasso webhook

--- webhooks/helloasso.js
+++ webhooks/helloasso.js
@@ -8,7 +8,45 @@ const router = express.Router();
 router.post('/helloasso', (req, res) => {
   const { data, eventType } = req.body;
 
-  if (eventType === 'Payment') {
+  if (eventType === 'Order') {
+    const { payer, items } = data;
+
+    const membership = items.find((item) => item.type === 'Membership');
+    const donation = items.find((item) => item.type === 'Donation');
+
+    if (membership) {
+      // membership + maybe donation
+      // push data to discord
+      helloasso.emit('membership', data);
+
+      // push data to OBS alerts
+      wss.sendMessage({
+        type: 'helloasso',
+        data: {
+          type: 'membership',
+          name: payer.firstName,
+          donation: donation ? donation.amount / 100 : undefined,
+          comment: membership.customFields.find((f) =>
+            f.name.includes('Remarques')
+          )?.answer,
+        },
+      });
+    } else if (donation) {
+      // only donation
+      // push data to discord
+      helloasso.emit('donation', data);
+
+      // push data to OBS alerts
+      wss.sendMessage({
+        type: 'helloasso',
+        data: {
+          type: 'donation',
+          name: payer.firstName,
+          donation: donation.amount / 100,
+        },
+      });
+    }
+  } else if (eventType === 'Payment') {
     const { payer, items } = data;
 
     const membership = items.find((item) => item.type === 'Membership');
@@ -38,6 +76,8 @@ router.post('/helloasso', (req, res) => {
         },
       });
     }
+  } else if (eventType === 'MembershipExpired') {
+    helloasso.emit('membership.expired', data);
   }
 
   res.sendStatus(204);
